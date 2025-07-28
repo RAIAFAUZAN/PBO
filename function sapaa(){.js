df29c7159a778f347a3ee64927eb668908cefb6e
KARAX <muhamadfauzanraia04@gmail.com>
2025-07-28 15:00:24 +0700

    BELAJAR JAVASCRIPT

---
+++ function sapaa(){.js
@@ -0,0 +1,49 @@
+// function sapa(nama) {
+// console.log("halo," + nama + "!");
+// }
+
+// sapa("nico");
+// sapa("loki");
+
+// let car = {
+// name: "fiat",
+// model: 500,
+// weight: "850kg",
+
+// start: function () {
+// console.log("mobil dinyalakan.");
+// },
+// drive: function () {
+// console.log("mobil berjalan");
+// },
+// brake: function () {
+// console.log("mobil mengerem.");
+// },
+// stop: function () {
+// console.log("mobil berhenti.");
+// },
+// };
+
+// console.log(car.name);
+// console.log(car.color);
+
+// car.start();
+// car.drive();
+
+let bicycle = {
+  name: "united",
+  gear: "7",
+  warnasepeda: "red",
+  start: function () {
+    console.log("sepeda mulai dikayuh");
+  },
+  brake: function () {
+    console.log("sepeda mengerem");
+  },
+};
+
+console.log(bicycle.name);
+console.log(bicycle.color);
+
+bicycle.start();
+bicycle.brake();
